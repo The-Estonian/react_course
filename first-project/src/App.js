8ef272849e4551d5de9d7dbde4fe05032c0fb5e7
The-Estonian <Zaar2213@gmail.com>
2022-09-04 19:31:39 +0300

    first project started

---
+++ first-project/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem"
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
