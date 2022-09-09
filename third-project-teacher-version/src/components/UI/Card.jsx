35255073159b997e297b2d5e3ba5c69745961cd5
The-Estonian <Zaar2213@gmail.com>
2022-09-09 15:11:36 +0300

    Created and codealong teacher version of third project

---
+++ third-project-teacher-version/src/components/UI/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./Card.module.css"
+
+const Card = (props) => {
+    let classes = styles.card + " " + props.className
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
