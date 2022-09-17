81087f11e5f73c8031e0388479500c74bd819067
The-Estonian <Zaar2213@gmail.com>
2022-09-18 00:29:55 +0300

    added error handling to server connection

--- sixth-project-react-with-backend/src/App.js
+++ sixth-project-react-with-backend/src/App.js
@@ -4,13 +4,46 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [connectionError, setConnectionError] = useState(null);
+
   const fetchMoviesHandler = async () => {
-    const response = await fetch("https://swapi.dev/api/films");
-    const moviesList = await response.json()
-    console.log(moviesList.results);
-    setMovies(moviesList.results)
+    setIsLoading(true);
+    setConnectionError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      
+      if (!response.ok) {
+        throw new Error('Did not get any data from that address!');
+      }
+      const moviesList = await response.json();
+      setMovies(moviesList.results);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error.message);
+      setConnectionError(error.message);
+    }
   };
+  // const fetchMoviesHandler = () => {
+  //   fetch('https://swapi.dev/api/films')
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       const dataTransformation = data.results.map(dataPoint => {
+  //         return {
+  //           episode_id: dataPoint.episode_id,
+  //           title: dataPoint.title,
+  //           director: dataPoint.director,
+  //           release_date: dataPoint.release_date,
+  //           opening_crawl: dataPoint.opening_crawl
+  //         }
+  //       })
+  //       setMovies(dataTransformation)
+  //     });
+  // };
 
   return (
     <React.Fragment>
@@ -18,7 +51,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length < 1 && !connectionError && <p>Fetch to get movies!</p>}
+        {isLoading && <p>Loading...</p>}
+        {connectionError && <p>{connectionError}</p>}
       </section>
     </React.Fragment>
   );
