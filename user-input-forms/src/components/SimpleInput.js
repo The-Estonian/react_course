88416ed0490d150bfa7c76b2f1af1097feccd9d6
The-Estonian <Zaar2213@gmail.com>
2022-09-19 21:45:07 +0300

    Introducing forms and validations

---
+++ user-input-forms/src/components/SimpleInput.js
@@ -0,0 +1,31 @@
+import { useRef, useState } from 'react';
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+    setEnteredName("")
+  };
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className='form-control'>
+        <label htmlFor='name'>Your Name</label>
+        <input ref={nameInputRef} value={enteredName} type='text' id='name' onChange={nameInputChangeHandler} />
+      </div>
+      <div className='form-actions'>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
