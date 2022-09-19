9c12873944d3ff08a30aa188e2ff1aa1e0a7ab23
The-Estonian <Zaar2213@gmail.com>
2022-09-19 18:05:25 +0300

    custom hook finished

--- hooks-connecting-server/src/App.js
+++ hooks-connecting-server/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import useServerFetchHook from './hooks/server-fetch-hook';
 
 import Tasks from './components/Tasks/Tasks';
@@ -9,43 +9,19 @@ function App() {
   const connection = {
     url: 'https://testing-react-96b3e-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
   };
-  const fetchServerData = (datastream) => {
+  const fetchServerData = useCallback((datastream) => {
     const loadedTasks = [];
     for (const taskKey in datastream) {
       loadedTasks.push({ id: taskKey, text: datastream[taskKey].text });
     }
     setTasks(loadedTasks);
-  };
-  const { isLoading, error, sendRequest: fetchTasks} = useServerFetchHook(connection, fetchServerData);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
+  }, []);
 
-  // const fetchTasks = async (taskText) => {
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch(
-  //       'https://testing-react-96b3e-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error('Request failed!');
-  //     }
-
-  //     const data = await response.json();
-
-  //     const loadedTasks = [];
-
-  //     for (const taskKey in data) {
-  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-  //     }
-
-  //     setTasks(loadedTasks);
-  //   } catch (err) {
-  //     setError(err.message || 'Something went wrong!');
-  //   }
-  //   setIsLoading(false);
-  // };
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useServerFetchHook(connection, fetchServerData);
 
   useEffect(() => {
     fetchTasks();
--- hooks-connecting-server/src/hooks/server-fetch-hook.jsx
+++ hooks-connecting-server/src/hooks/server-fetch-hook.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 const useServerFetchHook = (requestConfig, applyData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async () => {
+  const sendRequest = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -24,7 +24,7 @@ const useServerFetchHook = (requestConfig, applyData) => {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  }, [requestConfig, applyData]);
   return {
     isLoading,
     error,
