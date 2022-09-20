cd4639256d8cf1e5b53a636b0e0085b21715cd94
The-Estonian <Zaar2213@gmail.com>
2022-09-20 22:30:34 +0300

    implemented email input with verification

--- user-input-forms/src/components/SimpleInput.js
+++ user-input-forms/src/components/SimpleInput.js
@@ -1,26 +1,24 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
   const [nameInputError, setNameInputError] = useState(false);
+  const [emailInputError, setEmailInputError] = useState(false);
   // const [formIsValid, setFormIsValid] = useState(false);
 
-
-  let formIsValid = false
-  if (nameInputError) {
-    formIsValid = true
+  let formIsValid = false;
+  if (nameInputError && emailInputError) {
+    formIsValid = true;
   }
-  // useEffect(() => {
-  //   if (nameInputError) {
-  //     setFormIsValid(true);
-  //   } else {
-  //     setFormIsValid(false)
-  //   }
-  // }, [nameInputError]);
-
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
     setNameInputError(false);
+  };
+
+  const emailInputChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);
+    setEmailInputError(false);
   };
 
   const formSubmissionHandler = (e) => {
@@ -31,33 +29,62 @@ const SimpleInput = (props) => {
     } else {
       setNameInputError(false);
     }
+    if (!enteredEmail.includes("@")) {
+      setEmailInputError(true);
+    } else {
+      setEmailInputError(false);
+    }
+    setEnteredEmail('');
     setEnteredName('');
   };
 
-  const onBlurChangeHandler = () => {
+  const onBlurNameChangeHandler = () => {
     if (enteredName.trim().length < 1) {
       setNameInputError(true);
     } else {
       setNameInputError(false);
     }
   };
+  const onBlurEmailChangeHandler = () => {
+    if (!enteredEmail.includes("@")) {
+      setEmailInputError(true);
+    } else {
+      setEmailInputError(false);
+    }
+  };
 
-  const classes = nameInputError ? 'form-control invalid' : 'form-control';
+  const nameClass = nameInputError ? 'form-control invalid' : 'form-control';
+  const emailClass = emailInputError ? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className={classes}>
+      <div className={nameClass}>
         <label htmlFor='name'>Your Name</label>
         <input
           value={enteredName}
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
-          onBlur={onBlurChangeHandler}
+          onBlur={onBlurNameChangeHandler}
         />
       </div>
       {nameInputError ? (
         <p className='error-text'>Entered name is invalid!</p>
+      ) : (
+        ''
+      )}
+      <div className={emailClass}>
+        <label htmlFor='email'>Your Email</label>
+        <input
+          value={enteredEmail}
+          type='text'
+          id='email'
+          onChange={emailInputChangeHandler}
+          onBlur={onBlurEmailChangeHandler}
+        />
+      </div>
+      {emailInputError ? (
+        <p className='error-text'>Entered email is invalid!</p>
       ) : (
         ''
       )}
