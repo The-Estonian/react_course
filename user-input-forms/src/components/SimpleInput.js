3f861c6487410844e474972d42af8ead7d9b740b
The-Estonian <Zaar2213@gmail.com>
2022-09-20 20:08:42 +0300

    input validation done

--- user-input-forms/src/components/SimpleInput.js
+++ user-input-forms/src/components/SimpleInput.js
@@ -1,26 +1,48 @@
 import { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [inputError, setInputError] = useState(false);
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
+    setInputError(false)
   };
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
-
-    console.log(enteredName);
-    console.log(nameInputRef.current.value);
-    setEnteredName("")
+    if (enteredName.trim().length < 1) {
+      setInputError(true);
+      return;
+    } else {
+      setInputError(false);
+    }
+    setEnteredName('');
   };
+
+  const onBlurChangeHandler = () => {
+    if (enteredName.trim().length < 1) {
+      setInputError(true);
+    } else {
+      setInputError(false);
+    }
+  }
+
+  const classes = inputError ? "form-control invalid":"form-control"
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={classes}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} value={enteredName} type='text' id='name' onChange={nameInputChangeHandler} />
+        <input
+          value={enteredName}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          onBlur={onBlurChangeHandler}
+        />
       </div>
+      {inputError ? <p className="error-text">Entered name is invalid!</p> : ""}
       <div className='form-actions'>
         <button>Submit</button>
       </div>
