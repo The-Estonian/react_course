1147ef0713cecdf3cff946f7d15f8e22e4a86ec3
The-Estonian <Zaar2213@gmail.com>
2022-09-27 20:27:17 +0300

    basic e-shop with redux practice done

---
+++ refresher-one/src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My First Book',
+    description: 'My first book i ever wrote!',
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: 'My second book i ever wrote!',
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
---
+++ refresher-one/src/store/redux-logic.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import uiSlice from './ui-slice';
+import cartActions from './cart-slice';
+
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer, cart: cartActions.reducer },
+});
+
+export default store;
