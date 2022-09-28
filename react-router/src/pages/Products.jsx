79102b42c25d6af0b6560925b9e1d56d7b6241e4
The-Estonian <Zaar2213@gmail.com>
2022-09-28 21:08:44 +0300

    introducing react router

---
+++ react-router/src/pages/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Products = () => {
+  return (
+    <section>
+      <h1>Product page!</h1>
+      <ul>
+        <li><Link to="product/p1">A Book</Link></li>
+        <li><Link to="product/p2">A Carpet</Link></li>
+        <li><Link to="product/p3">An Online Course</Link></li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
