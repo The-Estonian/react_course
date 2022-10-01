eeb009de53ccc4d23cac374f4cf81e84f8b78f08
The-Estonian <Zaar2213@gmail.com>
2022-10-01 13:37:39 +0300

    added url and sort logic

--- router-practice/src/pages/NewQuote.jsx
+++ router-practice/src/pages/NewQuote.jsx
@@ -1,11 +1,14 @@
+import { useHistory } from 'react-router-dom';
 import React from 'react';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
   const onAddQuoteHandler = (data) => {
     console.log(data);
+    history.push("/quotes")
   };
-  return <QuoteForm onAddQuote={onAddQuoteHandler}/>;
+  return <QuoteForm onAddQuote={onAddQuoteHandler} />;
 };
 
 export default NewQuote;
