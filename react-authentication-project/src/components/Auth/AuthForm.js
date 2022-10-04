1fbd713bf12a2ca1873392be89eec77cfb9e2528
The-Estonian <Zaar2213@gmail.com>
2022-10-04 12:37:46 +0300

    react authentication project with firebase as backend

---
+++ react-authentication-project/src/components/Auth/AuthForm.js
@@ -0,0 +1,98 @@
+import { useState, useRef } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+
+    setIsLoading(true)
+    if (isLogin) {
+      // Login to account
+      fetch("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDeI4Oa2xDdpPcDRp5mG7dUkKBgMm1Yqbo")
+    } else {
+      // Create account
+      fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDeI4Oa2xDdpPcDRp5mG7dUkKBgMm1Yqbo',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((res) => {
+        setIsLoading(false)
+        if (res.ok) {
+          // connection made
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            if (data.error.message) {
+              console.log(data.error.message.split(':'));
+            }
+            if (data && data.error && data.error.message) {
+              if (data.error.message.split(':').length === 1) {
+                errorMessage =
+                  data.error.message.replace('_', ' ').charAt(0) +
+                  data.error.message.replace('_', ' ').slice(1).toLowerCase();
+              } else {
+                errorMessage = data.error.message.split(':')[1];
+              }
+            }
+            setError(true);
+            setErrorMessage(errorMessage);
+          });
+        }
+      });
+    }
+    
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' ref={emailRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' ref={passwordRef} required />
+        </div>
+        <div className={classes.actions}>
+          {isLoading && <p className={classes.error}>Connecting to server...</p>}
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {error && <p className={classes.error}>{errorMessage}</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
